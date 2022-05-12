035426fd5e9811157f2a2bd04c4e767be3b21296
Shinsina <shinsina@att.net>
2022-05-12 14:51:24 -0500

    Basic “site crawler” script.

---
+++ siteCrawler/index.js
@@ -0,0 +1,42 @@
+import puppeteer from 'puppeteer';
+import * as cheerio from 'cheerio';
+import striptags from 'striptags'
+
+async function crawl ({ url, root }) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setJavaScriptEnabled(false);
+  await page.goto(url, { waitUntil: 'networkidle0'});
+  await page.setViewport({ width: 1800, height: 900 });
+  const article = await page.$(root);
+  const html = await page.evaluate((article) => article.innerHTML, article);
+  await browser.close();
+  return { url, html }
+};
+
+const crawled = await crawl({ url: 'https://shinsina.github.io', root: 'body' });
+
+const { url, html } = crawled;
+
+const $ = cheerio.load(html, { }, false);
+
+const uniqueURL = new Set();
+
+$('*').each(function fn() {
+  if ('script' !== this.name) {
+    if (['img', 'a', 'iframe'].includes(this.name)) {
+      uniqueURL.add(this.attribs['data-src'] || this.attribs.href || this.attribs.src);
+      if (this.attribs['data-src'] || this.attribs.src) {
+        $(this).replaceWith($(`<a href="${this.attribs['data-src']||this.attribs.src}" target="_blank">Link</a> `))
+      }
+    }
+  }
+});
+
+const articleData = {
+  body: striptags(String($.html()), ['a', 'table', 'tr', 'td', 'th']).replace(/\n/g, '<br/>').replace(/  /g, '').replace(/(<br\/>)+/g, '<br/>'),
+  URLS: Array.from(uniqueURL),
+  url
+}
+
+console.log(articleData);
