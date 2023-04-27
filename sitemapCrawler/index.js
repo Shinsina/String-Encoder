477cd40f5c5571d1349d1b446201a524cc47ef6d
Jake Collins <shinsina@att.net>
2023-04-27 16:19:32 -0500

    Add crawling support for a folder of sitemaps

--- sitemapCrawler/index.js
+++ sitemapCrawler/index.js
@@ -9,32 +9,28 @@ const urlsToCrawl = new Set();
 
 const crawlSiteMap = async ({
   sitemapURL,
-  isXML,
   replacesWWW,
   iteration,
 }) => {
   log(`Handling iteration ${iteration}...`);
   const response = await fetch(sitemapURL);
   const sitemap = await response.text();
-  const $ = cheerio.load(sitemap, { xmlMode: isXML });
+  const $ = cheerio.load(sitemap, { xmlMode: true });
   const foundSitemaps = new Set();
-  if (isXML) {
-    $('loc').each((_, tag) => {
-      const [containsURL] = tag.children;
-      const { data: url } = containsURL;
-      if (url.match(/\.xml$/)) {
-        foundSitemaps.add(url);
-      } else {
-        const urlToCrawl = replacesWWW ? url.replace('www', replacesWWW) : url;
-        urlsToCrawl.add(urlToCrawl);
-      }
-    });
-  }
+  $('loc').each((_, tag) => {
+    const [containsURL] = tag.children;
+    const { data: url } = containsURL;
+    if (url.match(/\.xml$/)) {
+      foundSitemaps.add(url);
+    } else {
+      const urlToCrawl = replacesWWW ? url.replace('www', replacesWWW) : url;
+      urlsToCrawl.add(urlToCrawl);
+    }
+  });
   const sitemaps = Array.from(foundSitemaps);
   if (sitemaps.length) {
     await Promise.all(sitemaps.map((url) => crawlSiteMap({
       sitemapURL: url,
-      isXML,
       replacesWWW,
       iteration: iteration + 1,
     })));
@@ -42,17 +38,36 @@ const crawlSiteMap = async ({
   return null;
 };
 
+const parseSiteMap = ({ file, replacesWWW }) => {
+  const raw = fs.readFileSync(file).toString();
+  const $ = cheerio.load(raw, { xmlMode: true });
+  $('loc').each((_, tag) => {
+    const [containsURL] = tag.children;
+    const { data: url } = containsURL;
+    const urlToCrawl = replacesWWW ? url.replace('www', replacesWWW) : url;
+    urlsToCrawl.add(urlToCrawl);
+  });
+};
+
 const main = async () => {
-  const { sitemapURL, isXML, replacesWWW } = await inquirer.prompt([
+  const answers = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'isURL',
+      message: 'Is the source for the sitemap a URL?',
+    },
     {
       type: 'input',
       name: 'sitemapURL',
       message: 'Enter the URL for the sitemap to process (with protocol)',
+      when: ({ isURL }) => isURL,
     },
     {
-      type: 'confirm',
-      name: 'isXML',
-      message: 'Is the sitemap in XML format?',
+      type: 'input',
+      name: 'siteMapsRelativePath',
+      message: 'Enter the relative folder for the sitemaps to process',
+      default: './input',
+      when: ({ isURL }) => !isURL,
     },
     {
       type: 'input',
@@ -60,13 +75,20 @@ const main = async () => {
       message: 'What should replace www on the URLs to crawl? (Optional)',
     },
   ]);
-  const domainName = sitemapURL.split('/')[2];
-  await crawlSiteMap({
-    sitemapURL,
-    isXML,
-    replacesWWW,
-    iteration: 1,
-  });
+  const { sitemapURL, siteMapsRelativePath, replacesWWW } = answers;
+  const domainName = sitemapURL ? sitemapURL.split('/')[2] : siteMapsRelativePath.split('/').find((value) => value.match(/\w+/));
+  if (sitemapURL) {
+    await crawlSiteMap({
+      sitemapURL,
+      replacesWWW,
+      iteration: 1,
+    });
+  } else {
+    fs.readdirSync(siteMapsRelativePath).forEach((file) => {
+      parseSiteMap({ file: `${siteMapsRelativePath}/${file}`, replacesWWW });
+    });
+  }
+
   const outputRelativePath = `./output/${domainName}`;
   if (!fs.existsSync(outputRelativePath)) {
     fs.mkdirSync(outputRelativePath);
