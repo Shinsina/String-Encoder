64deec99aa8d6c80d2351feb610ad37bc475eccc
Jake Collins <shinsina@att.net>
2023-04-27 15:44:19 -0500

    Utilize new withBrowser and withPage utils

--- checkIfPageExists/index.js
+++ checkIfPageExists/index.js
@@ -1,27 +1,9 @@
 import fs from 'fs';
 import inquirer from 'inquirer';
-import puppeteer from 'puppeteer';
 import bluebird from 'bluebird';
+import { withBrowser, withPage } from '../utils/puppeteer-utils.js';
 
 const { log } = console;
-
-const withBrowser = async (fn) => {
-  const browser = await puppeteer.launch({/* ... */});
-  try {
-    return await fn(browser);
-  } finally {
-    await browser.close();
-  }
-};
-
-const withPage = (browser) => async (fn) => {
-  const page = await browser.newPage();
-  try {
-    return await fn(page);
-  } finally {
-    await page.close();
-  }
-};
 
 const main = async () => {
   const { urlListFileName } = await inquirer.prompt([
